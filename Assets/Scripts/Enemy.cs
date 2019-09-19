28ac09597fd990525945b6e40a0dcec7fdc6e240
Brian Withrow <bdwithrow2115@eagle.fgcu.edu>
2019-09-19 19:40:53 -0400

    Adding response for Enemy
    Working on enemy being bumped into; Enemy and player need to store their initial positions for comparison.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     public Transform mesh;
+    public Transform player;
     public Coroutine currentCoroutine;
 
+    public float moveSpeed;
     public float initSNSpeed;
+    private Rigidbody rb;
+    public float hopHeight;
 
     //Controls degree of squish
     private float SquashDegree;
@@ -21,6 +25,7 @@ public class Enemy : MonoBehaviour {
         //Reset mesh values to an original state
         Reset();
 
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -68,20 +73,48 @@ public class Enemy : MonoBehaviour {
         Reset();
     }
 
+    public IEnumerator bumpedInto()
+    {
+        rb.velocity = new Vector3(moveSpeed, hopHeight, rb.velocity.z);
+
+        while (moveSpeed > 0)
+        {
+            rb.velocity = new Vector3(moveSpeed, rb.velocity.y, rb.velocity.z);
+
+            moveSpeed -= 5.0f * Time.deltaTime;
+
+            yield return null;
+        }
+
+        // Reel back to charge up
+        rb.velocity = -Vector3.right * moveSpeed / 5.0f;
+
+        //Wait until back in the reset position
+        yield return new WaitUntil(() => Vector3.Distance(transform.position, player.position) >= 10.0f);
+
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        
         //On collision trigger the squish effect
         if (collision.gameObject.tag == "Player")
         {
+            Vector3 collisionNormal =  collision.contacts[0].normal;
             //Cancel current coroutine if one is active
             if (currentCoroutine != null)
             {
                 StopCoroutine(currentCoroutine);
                 currentCoroutine = null;
             }
-            
-            currentCoroutine = StartCoroutine(SquashNStretch());
+
+            if (Vector3.Dot(collisionNormal, -Vector3.up) == 1.0f)
+            {
+                currentCoroutine = StartCoroutine(SquashNStretch());
+            }
+            else if (Vector3.Dot(collisionNormal, Vector3.right) == 1.0f)
+            {
+                currentCoroutine = StartCoroutine(bumpedInto());
+            }
         }
     }
 }
