d233fffd4c8f1994bd489c4df0cc46085f39faa8
Brian Withrow <bdwithrow2115@eagle.fgcu.edu>
2019-09-19 18:34:26 -0400

    Initial Commit
    Hey ma, all commits!!
    -Attack1 and 2 added
    -Smoke effect on Attk2
    -Retreat Function
    -Wobble effect on enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public Transform mesh;
+    public Coroutine currentCoroutine;
+
+    public float initSNSpeed;
+
+    //Controls degree of squish
+    private float SquashDegree;
+
+    //Controls speed of squishing eff
+    [SerializeField]
+    private float SNSpeed;
+
+	// Use this for initialization
+	void Start () {
+        //Reset mesh values to an original state
+        Reset();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void Reset()
+    {
+        //Reset values for before enemy is hit
+        SquashDegree = Mathf.PI / 2.0f;
+
+        SNSpeed = initSNSpeed;
+
+        mesh.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    public IEnumerator SquashNStretch()
+    {
+        //While loop for Squish Animation
+        while (SNSpeed > 0.0f)
+        {
+            mesh.localScale = new Vector3(1.0f, (Mathf.Sin(SquashDegree * 6.0f) / 10.0f) + 0.9f, 1.0f);
+
+            SNSpeed = Mathf.Clamp(SNSpeed, 0.0f, 10.0f);
+
+            SquashDegree += Time.deltaTime * SNSpeed;
+
+            SNSpeed -= Mathf.Pow(Time.deltaTime, 0.5f);
+
+            yield return null;
+        }
+
+        //Post squish returning to something close to reset state
+        while (mesh.localScale.y < 0.95f)
+        {
+            mesh.localScale = new Vector3(1.0f, (Mathf.Sin(SquashDegree * 6.0f) / 10.0f) + 0.9f, 1.0f);
+
+            SquashDegree += Time.deltaTime;
+
+            yield return null;
+        }
+
+        //Properly reset
+        Reset();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //On collision trigger the squish effect
+        if (collision.gameObject.tag == "Player")
+        {
+            //Cancel current coroutine if one is active
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+                currentCoroutine = null;
+            }
+
+            currentCoroutine = StartCoroutine(SquashNStretch());
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float hopHeight;
+    public bool attack;
+    public bool attackTwo;
+    public float n;
+    public ParticleSystem dust;
+
+    public Transform enemy;
+
+    private Coroutine currentCoroutine;
+    private Rigidbody rb;
+    private Vector3 dustLocalPosition;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        dustLocalPosition = dust.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (attack && currentCoroutine == null)
+        {
+            currentCoroutine = StartCoroutine(Attack());
+        }
+
+        if (attackTwo && currentCoroutine == null)
+        {
+            currentCoroutine = StartCoroutine(AttackTwo());
+        }
+    }
+
+    public IEnumerator Attack()
+    {
+        // move towards enemy
+        rb.velocity = Vector3.right * moveSpeed;
+
+        // wait until player is at a certain distance from enemy
+        yield return new WaitUntil(() => Vector3.Distance(transform.position, enemy.position) <= 5.0f);
+
+        // halt for 0.5s
+        rb.velocity = Vector3.zero;
+
+        yield return new WaitForSeconds(0.5f);
+
+        // hop
+        rb.velocity = new Vector3(moveSpeed, hopHeight, rb.velocity.z);
+
+        // wait until player is directly above enemy
+        yield return new WaitUntil(() => Mathf.Abs(transform.position.x - enemy.position.x) < 0.1f);
+
+        rb.velocity = new Vector3(0.0f, rb.velocity.y, rb.velocity.z);
+    }
+
+    public IEnumerator AttackTwo()
+    {
+        // Reel back to charge up
+        rb.velocity = -Vector3.right * moveSpeed / 5.0f;
+
+        //Increase in size while charging up
+
+        
+        while (Mathf.Abs(transform.position.x - enemy.position.x) < 12.0f)
+        {
+            n += 0.5f * Time.deltaTime;
+            transform.localScale = new Vector3(n*1.0f, n*1.0f, n*1.0f);
+            yield return null;
+        }
+
+        //The point where it should stop charging and start running
+        yield return new WaitUntil(() => Mathf.Abs(transform.position.x - enemy.position.x) >= 12.0f);
+        dust.Play();
+        
+        // move towards enemy
+        rb.velocity = Vector3.right * moveSpeed * 2.0f;
+        while (n > 1)
+        {
+            n -= 1.0f * Time.deltaTime;
+            transform.localScale = new Vector3(n * 1.0f, n * 1.0f, n * 1.0f);
+            yield return null;
+        }
+
+        // halt for 0.5s
+        //rb.velocity = Vector3.zero;
+
+    }
+
+    public IEnumerator Retreat()
+    {
+        // hop off
+        rb.velocity = new Vector3(-moveSpeed, hopHeight / 2, rb.velocity.z);
+
+        n = 1;
+        transform.localScale = new Vector3(n * 1.0f, n * 1.0f, n * 1.0f);
+
+        //Wait until back in the reset position
+        yield return new WaitUntil(() => Vector3.Distance(transform.position, enemy.position) >= 10.0f);
+
+        // halt for 0.5s
+        rb.velocity = Vector3.zero;
+
+        attack = false;
+        attackTwo = false;
+        currentCoroutine = null;
+        dust.transform.parent = transform;
+        dust.transform.localPosition = dustLocalPosition;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //On collision trigger the squish effect
+        if (collision.gameObject.tag == "Enemy")
+        {
+            dust.Stop();
+            dust.transform.parent = null;
+
+            //Cancel current coroutine if one is active
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+                currentCoroutine = null;
+            }
+
+            currentCoroutine = StartCoroutine(Retreat());
+        }
+    }
+}
