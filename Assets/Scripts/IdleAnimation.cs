9a2bebf18609222c37d9f000532f41e9834123e1
Gregorio Lozada <greglozada@gmail.com>
2019-09-24 16:52:08 -0400

    Update With Brian's Repo
    Updating repository

---
+++ Assets/Scripts/IdleAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleAnimation : MonoBehaviour
+{
+    private float n;
+    private float initY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        n = 0.0f;
+        initY = transform.localPosition.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = sinFunc(n, 2.0f, 0.05f);
+
+        transform.localScale = new Vector3(1.0f - (offset / 2.0f), 1.0f + offset, 1.0f);
+        transform.localPosition = new Vector3(transform.localPosition.x, initY + (offset / 2.0f), transform.localPosition.z);
+
+        n += Time.deltaTime;
+    }
+
+    float sinFunc(float x, float freq, float amp)
+    {
+        float theta = (freq * x) + (Mathf.PI / 2.0f);
+
+        return (Mathf.Sin(theta) * amp) - amp;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Vector3 target;
+    private bool facingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // look at target
+        Vector3 dir = Vector3.RotateTowards(transform.forward, target, 10 * Time.deltaTime, 0.0f);
+
+        transform.rotation = Quaternion.LookRotation(dir);
+
+        // right input
+        if (Input.GetAxis("Horizontal") > 0.0f)
+        {
+            // change target
+            if (!facingRight)
+            {
+                target = -target;
+                facingRight = true;
+            }
+        }
+        // left input
+        else if (Input.GetAxis("Horizontal") < 0.0f)
+        {
+            // change target
+            if (facingRight)
+            {
+                target = -target;
+                facingRight = false;
+            }
+        }
+    }
+}
